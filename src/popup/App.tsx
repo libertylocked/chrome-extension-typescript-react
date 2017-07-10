3de01b9cf147ef08faccf341e5461a94b1b0ffb8
libertylocked <unlock.liberty@gmail.com>
2017-07-09 21:44:33 -0400

    New React template for popup

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -5,18 +5,29 @@ const logo = require("./logo.svg");
 
 class App extends React.Component<{}, {}> {
   public render() {
-    const formattedTime: string = new Date().toString();
+    const formattedTime: string = new Date().toLocaleTimeString("en-us", {
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      month: "numeric",
+      second: "2-digit",
+      year: "numeric",
+    });
 
     return (
-      <div className="App-container">
-        <h1 className="App-title">My Awesome Chrome Extension</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <span>React + TypeScript!</span>
-        <p>{formattedTime}</p>
-        <ul>
-          <li>Right click and select <strong>Inspect</strong> to open DevTools</li>
-          <li>In DevTools, press Ctrl+R to reload</li>
-        </ul>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/popup/App.js</code>
+        </p>
+        <p className="App-intro">
+          Right click and select <strong>Inspect</strong> to open DevTools.
+          In DevTools, press <code>Ctrl+R</code> to reload
+        </p>
+        <p>Rendered at {formattedTime}</p>
       </div>
     );
   }
