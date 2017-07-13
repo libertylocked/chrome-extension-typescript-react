d250514367fe5d7028b4509d63ec15dc4a34878b
libertylocked <unlock.liberty@gmail.com>
2017-07-13 02:57:44 -0400

    Add jest for testing. Update packages and scripts

---
+++ __mocks__/fileTransform.ts
@@ -0,0 +1,8 @@
+const path = require("path");
+
+// This is a custom Jest transformer turning file imports into filenames.
+// http://facebook.github.io/jest/docs/tutorial-webpack.html
+
+export function process(src: any, filename: string) {
+  return `module.exports = ${JSON.stringify(path.basename(filename))};`;
+}
