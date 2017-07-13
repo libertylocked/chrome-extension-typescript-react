56a4482e8888e40996df2e87d9380a210e81a79d
libertylocked <unlock.liberty@gmail.com>
2017-07-13 13:56:23 -0400

    Move file transform to config/jest

--- config/jest/file-transform.ts
+++ config/jest/file-transform.ts
@@ -1,4 +1,4 @@
-import path from "path";
+import * as path from "path";
 
 // This is a custom Jest transformer turning file imports into filenames.
 // http://facebook.github.io/jest/docs/tutorial-webpack.html
